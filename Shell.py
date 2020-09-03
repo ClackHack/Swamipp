fbac8440c3c7983eeaed6b411a8d67c66f710c98
ClackHack <48455071+ClackHack@users.noreply.github.com>
2020-09-03 13:53:37 -0400

    Swami++ 2.0
    Added support for classes
    Refactored dictionary.spp to be class

--- Shell.py
+++ Shell.py
@@ -1,10 +1,29 @@
 import Swamipp as Swami
 import os,datetime
-import _thread
+import _thread, sys
+import argparse
+parser = argparse.ArgumentParser(description="Swami++")
+parser.add_argument("file",nargs="?",type=str,help="file to be executed",default="")
+args=parser.parse_args()
+if args.file:
+  try:
+    code=open(args.file,"r").read()
+    y=datetime.datetime.now()
+    result,error=Swami.run(args.file,code)
+    x=datetime.datetime.now()
+    if error:
+        print(error.toString(),sep="\n")
+    else:
+        print(f"\nExecuted with zero errors in {(x-y).total_seconds()} seconds")
+  except KeyboardInterrupt:
+    sys.exit()
+  except Exception as e:
+    print("Could not find file, or fatal error...",e)
+  sys.exit()
 def begin(s,r):
   return s[:len(r)]==r
-print("Swami++ 1.2, type credits for more info")
-directory="Programs/"
+print("Swami++ 2.0, type credits for more info")
+directory="C:/Swamipp/Programs/"
 def notepad(f):
     os.system("notepad.exe "+directory+f)
     return
